7576cbcdeca27841ba4a900d483280f0d7d416b1
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-09-17 17:49:22 +0200

    Use Bootstrap form and form grid

--- src/components/searchBar.tsx
+++ src/components/searchBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown';
+import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './searchBar.css';
@@ -9,55 +10,66 @@ import './searchBar.css';
 type Inputs = {
     searchField: string;
     Location: string;
+    Skill: String;
+    Gender: String;
 };
 
 /*
     USES: 
     https://react-bootstrap.github.io/components/DropdownButton/
-    https://react-bootstrap.github.io/components/Dropdown/
+    https://react-bootstrap.github.io/components/dropdowns/
     https://react-bootstrap.github.io/components/forms/
     https://react-bootstrap.github.io/components/buttons/
-
+    https://react-bootstrap.github.io/components/forms/
     
 */
 
-enum locations {
-    'Oslo/Viken' = '#/action-1',
-    'Trønderlag' = '#/action-2',
-    'North Norway' = '#/action-3',
-    'West Norway' = '#/action-4',
-    'South Norway' = '#/action-5',
-}
-
 const SearchBar = () => {
     const [dropdownLocation, setDropdownLocation] = useState('Location');
+    const [dropdownSkill, setDropdownSkill] = useState('All skill');
+    const [dropdownGender, setDropdownGender] = useState('All gender');
+
     return (
         <div className="searchBar">
             <Form>
-                <Form.Group controlId="searchField">
-                    <Form.Control type="string" placeholder="Search..." />
-                </Form.Group>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="searchField">
+                        <Form.Control type="string" placeholder="Search..." />
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="locationField">
+                        <Form.Control as="select" placeholder="Location">
+                            <option>Northern Norway</option>
+                            <option>Eastern Norway</option>
+                            <option>Western Norway</option>
+                            <option>Southern Norway</option>
+                            <option>Oslo/Viken Norway</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    <Form.Group as={Col} controlId="SkillField">
+                        <Form.Control as="select" placeholder="Skill level">
+                            <option>Beginner</option>
+                            <option>Skilled</option>
+                            <option>Professional</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <DropdownButton
-                    id="dropdown-location"
-                    title={dropdownLocation}
-                    className="dropdown-location"
-                    onSelect={(selected) => {
-                        if (selected) {
-                            setDropdownLocation(selected);
-                        }
-                    }}
-                >
-                    <Dropdown.Item eventKey="Northern Norway">Northern Norway</Dropdown.Item>
-                    <Dropdown.Item eventKey="Central Norway">Eastern Norway</Dropdown.Item>
-                    <Dropdown.Item eventKey="Western Norway">Western Norway</Dropdown.Item>
-                    <Dropdown.Item eventKey="Southern Norway">Southern Norway</Dropdown.Item>
-                    <Dropdown.Item eventKey="Eastern Norway">Eastern Norway</Dropdown.Item>
-                </DropdownButton>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
+                    <Form.Group as={Col} controlId="GenderField">
+                        <Form.Control as="select" placeholder="Skill level">
+                            <option>Woman's team</option>
+                            <option>Men's team</option>
+                            <option>Mixed team</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                    <Button variant="primary" type="submit">
+                        Show results
+                    </Button>
+                </Form.Row>
             </Form>
         </div>
     );
