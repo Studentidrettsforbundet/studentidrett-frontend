0d476cc1378fce8f165ba5abe41ddb0678da49b2
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-09-24 15:46:15 +0200

    Move Api calls from components to services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,15 @@
-export const simpleSearch = (queryUrl: string) => {};
+import { CLUB, cardType } from '../constants';
+import { UrlBuilderSimpleSearch } from './urlBuilders';
+
+export const simpleSearch = (queryUrl: string, cardType: cardType) => {
+    return fetch(UrlBuilderSimpleSearch(cardType, queryUrl), {
+        method: 'GET',
+        headers: new Headers({
+            Accept: 'application/json',
+        }),
+    })
+        .then((res) => res.json())
+        .then((result) => {
+            return result;
+        });
+};
