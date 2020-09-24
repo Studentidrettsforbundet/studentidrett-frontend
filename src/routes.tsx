953a94ef10dec0c57cc07907f2852e0a3828471a
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-09-24 13:04:13 +0200

    Add the last two pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
-import LocationsPage from './pages/LocationsPage'
-import SportsPage from './pages/SportsPage'
+import ClubPage from './pages/ClubPage';
+import LocationsPage from './pages/LocationsPage';
+import SportsPage from './pages/SportsPage';
+import TeamPage from './pages/TeamPage';
 
 const createRoutes = () => (
     <Router>
@@ -11,7 +13,9 @@ const createRoutes = () => (
                 <Route exact path="/" component={App} />
                 <Route exact path="/Locations" component={LocationsPage} />
                 <Route exact path="/:Locations" component={SportsPage} />
-             </Switch>
+                <Route exact path="/:Locations/:Sports" component={ClubPage} />
+                <Route exact path="/:Locations/:Sports/:Clubs" component={TeamPage} />
+            </Switch>
         </div>
     </Router>
 );
