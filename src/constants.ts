777c9771ed411670e3162e22758b1d4595ddc2cc
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-10-06 16:45:41 +0200

    Update base url and constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,10 @@
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'https://kundestyrt-nsi-dev-backend.azurewebsites.net/';
 
 export const CLUB = 'clubs';
 export const SPORT = 'sports';
 export const TEAM = 'teams';
+export const REGION = 'regions';
+export const CITY = 'cities';
+export const GROUP = 'groups';
 
-export type cardType = typeof CLUB | typeof SPORT | typeof TEAM;
+export type cardType = typeof CLUB | typeof SPORT | typeof TEAM | typeof REGION | typeof CITY | typeof GROUP;
