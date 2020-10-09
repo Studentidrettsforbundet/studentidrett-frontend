8b0a9964fa4905b35e63aaad20a71b667ff1e93b
edvardag <edvard.groenvoll@gmail.com>
2020-10-09 11:11:33 +0200

    Add snapshot testing for pages and components

---
+++ src/__tests__/components/regionCard.test.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
+import { MemoryRouter } from 'react-router-dom';
+import RegionCard from '../../components/regionCard';
+import store from '../../store/store';
+
+
+describe('regionCard', () => {
+    test("renders correctly", () => {
+        const tree = renderer
+            .create(<Provider store={store}>
+                <MemoryRouter>
+                    <RegionCard id={0} name={"MidtNorge"} />
+                </MemoryRouter>
+            </Provider>);
+        expect(tree).toMatchSnapshot();
+    });
+});
---
+++ src/__tests__/routes.test.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Link, MemoryRouter } from 'react-router-dom';
+
+
+describe('routes.tsx', () => {
+    test("renders home Link correctly", () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Link to={"/Regions"} key={0} className={'unstyled_link'}/>
+            </MemoryRouter>);
+        expect(tree).toMatchSnapshot();
+    });
+    test("renders region Link correctly", () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Link to={"/NordNorge"} key={0} className={'unstyled_link'} />
+            </MemoryRouter>);
+        expect(tree).toMatchSnapshot();
+    });
+    test("renders sport Link correctly", () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Link to={"NordNorge/Fotball"} key={0} className={'unstyled_link'} />
+            </MemoryRouter>);
+        expect(tree).toMatchSnapshot();
+    });
+    test("renders club Link correctly", () => {
+        const tree = renderer
+            .create(<MemoryRouter>
+                <Link to={"MidtNorge/Fotball/NTNUIFotball"} key={0} className={'unstyled_link'} />
+            </MemoryRouter>);
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+
