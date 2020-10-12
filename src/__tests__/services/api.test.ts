6f826fb3b548bae5ba1592a901e762a7fdfaeed0
lillesol <olavsole@gmail.com>
2020-10-12 16:40:06 +0200

    Simple testsuite for API calls with mocked API implemented with fetch… (#13)
    * Simple testsuite for API calls with mocked API implemented with fetch-mock
    
    * Changed from strings to consts
    
    * added test for all card types
    
    Co-authored-by: KristofferHaakonsen <kristofferhhaakonsen@gmail.com>

--- src/__tests__/services/api.test.ts
+++ src/__tests__/services/api.test.ts
@@ -1,5 +1,161 @@
-import React from 'react';
+import fetchMock from 'fetch-mock';
+import { error } from 'console';
+import { fetchData, simpleSearch } from '../../services/api';
+import { urlBuilderFetchData, urlBuilderSimpleSearch } from '../../services/urlBuilders';
+import { cityInterface, sportInterface, regionInterface, teamInterface, clubInterface, groupInterface } from '../../interfaces';
+import { CITY, CLUB, SPORT, GROUP, TEAM, REGION } from '../../constants';
 
-describe('simpleSearch', () => {});
+// This test need libraries node-fetch and fetch-mock installed
+describe('simpleSearch whitebox test with "fetch-mock"', () => {
+    beforeEach(() => {
+        fetchMock.restore();
+    })
 
-describe('fetchData', () => {});
+    test('Valid Fetch (Fetches one sport)', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<sportInterface>(),
+        };
+        fetchMock.mock(urlBuilderSimpleSearch(SPORT, "fotball" ), { body: mockData, status: 200 });
+        const res = await simpleSearch("fotball",SPORT);
+        expect(res).toEqual(mockData);
+        
+    });
+
+    test('Returns empty object when status != 200', async () => {
+        const mockData = {
+            count: 0,
+            next: null,
+            previous: null,
+            results: [],
+        };
+        fetchMock.mock(urlBuilderSimpleSearch(CITY, "sasdas"), { status: 404 });
+        const res = await simpleSearch("sasdas", CITY);
+        expect(res).toEqual(mockData);
+        expect(res.results).toEqual(mockData.results)
+    });
+
+    test('Returns "Connection error" when fetch returns error', async () => {
+        fetchMock.mock(urlBuilderSimpleSearch(CLUB, "NTNUI"), error)
+        const res = await simpleSearch("NTNUI",CLUB);
+        expect(res).toEqual("Connection error");
+    });
+
+});
+
+describe('fetchData Whitebox test with fetchMock', () => {
+    beforeEach(() => {
+        fetchMock.restore();   
+    })
+   
+    test('Valid Fetch (Fetches one city)', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<cityInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(GROUP), { body: mockData, status: 200 });
+        const res = await fetchData(GROUP);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Returns empty object when status != 200', async () => {
+        const mockData = {
+            count: 0,
+            next: null,
+            previous: null,
+            results: [],
+        };
+        fetchMock.mock(urlBuilderFetchData(TEAM), { status: 404 });
+        const res = await fetchData(TEAM);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Returns "Connection error" when fetch returns error', async () => {
+        fetchMock.mock(urlBuilderFetchData(REGION), error)
+        const res = await fetchData(REGION);
+        expect(res).toEqual("Connection error");
+    });
+});
+
+describe('fetchData for all card types ', () => {
+    beforeEach(() => {
+        fetchMock.restore();
+    })
+
+    test('Valid Fetch for card CLUB', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<clubInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(CLUB), { body: mockData, status: 200 });
+        const res = await fetchData(CLUB);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Valid Fetch for card SPORT', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<sportInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(SPORT), { body: mockData, status: 200 });
+        const res = await fetchData(SPORT);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Valid Fetch for card TEAM', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<teamInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(TEAM), { body: mockData, status: 200 });
+        const res = await fetchData(TEAM);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Valid Fetch for card REGION', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array <regionInterface> (),
+        };
+        fetchMock.mock(urlBuilderFetchData(REGION), { body: mockData, status: 200 });
+        const res = await fetchData(REGION);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Valid Fetch for card CITY', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<cityInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(CITY), { body: mockData, status: 200 });
+        const res = await fetchData(CITY);
+        expect(res).toEqual(mockData);
+    });
+
+    test('Valid Fetch for card GROUP', async () => {
+        const mockData = {
+            count: 1,
+            next: null,
+            previous: null,
+            results: Array<groupInterface>(),
+        };
+        fetchMock.mock(urlBuilderFetchData(GROUP), { body: mockData, status: 200 });
+        const res = await fetchData(GROUP);
+        expect(res).toEqual(mockData);
+    });
+
+});
