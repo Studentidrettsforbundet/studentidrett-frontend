1ba6da553fe63db99e8978c44ebe41073782615c
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-10-26 08:38:23 +0100

    Cleanup code

--- src/pages/clubPage.tsx
+++ src/pages/clubPage.tsx
@@ -11,6 +11,7 @@ import { combinedStateInterface } from '../store/store';
 interface urlParams {
     Region: string;
     Sport: string;
+    Club: string;
 }
 
 const ClubPage = () => {
@@ -19,39 +20,45 @@ const ClubPage = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if (!reduxState.thunk.fetch_in_progress && reduxState.thunk.fetch_failed_count < 3 && !reduxState.thunk.fetch_success) {
+        if (
+            !reduxState.thunk.fetch_in_progress &&
+            reduxState.thunk.fetch_failed_count < 3 &&
+            !reduxState.thunk.fetch_success
+        ) {
             dispatch(fetchDataThunk(GROUP));
         }
     });
 
     const listContent = reduxState.group.groups.map((entry) => {
-        return <GroupCard{...{
-            id: entry.id,
-            name: entry.name,
-            description: entry.description,
-            cover_photo: entry.cover_photo,
-            sports: entry.sports,
-            club: entry.club,
-            city: entry.city,
-            contact_email: entry.contact_email,
-        }}  key = { entry.id } />
+        return (
+            <GroupCard
+                {...{
+                    id: entry.id,
+                    name: entry.name,
+                    description: entry.description,
+                    cover_photo: entry.cover_photo,
+                    sports: entry.sports,
+                    club: entry.club,
+                    city: entry.city,
+                    contact_email: entry.contact_email,
+                }}
+                key={entry.id}
+            />
+        );
     });
 
     return (
         <div className="container body">
             <div className="row">
                 <div className="col">
-                    <h1>Cities</h1>
+                    <h1>{urlParams.Club}</h1>
                 </div>
                 <div className="col search_icon-container">
                     <SearchIcon />
                 </div>
             </div>
             <SearchBar typeOfSearch={CLUB} />
-            <p>Select club</p>
-            <div className="card-columns">
-                {listContent}
-            </div>
+            <div className="card-columns">{listContent}</div>
         </div>
     );
 };
