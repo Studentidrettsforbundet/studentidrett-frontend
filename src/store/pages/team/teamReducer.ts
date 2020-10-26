1d878c1467c9dd307ab0df6f347e4e58ce5d46b5
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-10-26 11:38:51 +0100

    Remove placeholder data

--- src/store/pages/team/teamReducer.ts
+++ src/store/pages/team/teamReducer.ts
@@ -6,30 +6,7 @@ export interface teamState {
 }
 
 export const teamInitialState: teamState = {
-    teams: [
-        {
-            id: 0,
-            name: 'MEN A',
-            full_capacity: true,
-            short_description: 'Elite male team at NTNUI',
-            long_description:
-                'This is the elite men team at NTNUI football. Vi train 5 times a week and competite at a professional level',
-            tryouts: true,
-            registration_open: false,
-            group: 1,
-        },
-        {
-            id: 0,
-            name: 'Kvinner 96',
-            full_capacity: true,
-            short_description: 'Elite female team at NTNUI',
-            long_description:
-                'This is the elite female team at NTNUI football. Vi train 5 times a week and competite at a professional level',
-            tryouts: true,
-            registration_open: false,
-            group: 1,
-        },
-    ],
+    teams: [],
 };
 
 export const teamReducer = (state = teamInitialState, action: teamActionTypes): teamState => {
