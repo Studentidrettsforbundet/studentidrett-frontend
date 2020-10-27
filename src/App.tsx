766d200d946f8f050a2b3155896bd8768eda0b71
magdalenatran <tranmagdalena@gmail.com>
2020-10-27 22:27:20 +0100

    refactor app component to render pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './styles/global.css';
 import './assets/ko355.woff';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/footer';
+import Routes from './routes';
 
 const App = () => {
     return (
         <div className="App">
-            <h1>Kundestyrt</h1>
-            <div className="col">
-                <button type="button" className="btn btn-primary">
-                    <Link to="/Regions" style={{ color: 'black' }}>
-                        Regions
-                    </Link>
-                </button>
-                <button type="button" className="btn btn-primary">
-                    <Link to="/questionnaire" style={{ color: 'black' }}>
-                        Questionnaire
-                    </Link>
-                </button>
+            <div className="page">
+                <Routes />
             </div>
+            <Footer />
         </div>
     );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import App from './App';
 import RegionPage from './pages/regionPage';
 import CityPage from './pages/cityPage';
 import SportPage from './pages/sportPage';
@@ -10,24 +9,23 @@ import GroupPage from './pages/groupPage';
 import QuestionnairePage from './pages/questionnairePage';
 import ResultPage from './pages/resultPage';
 import store from './store/store';
+import LandingPage from './pages/landingPage';
 
-const createRoutes = () => (
+const Routes = () => (
     <Provider store={store}>
         <Router>
-            <div className="page">
-                <Switch>
-                    <Route exact path="/" component={App} />
-                    <Route exact path="/questionnaire" component={QuestionnairePage} />
-                    <Route exact path="/questionnaire/result" component={ResultPage} />
-                    <Route exact path="/Regions" component={RegionPage} />
-                    <Route exact path="/:City" component={CityPage} />
-                    <Route exact path="/:City/:Club" component={ClubPage} />
-                    <Route exact path="/:City/Sport/:Sport" component={SportPage} />
-                    <Route exact path="/:City/:Club/:Group" component={GroupPage} />
-                </Switch>
-            </div>
+            <Switch>
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/questionnaire" component={QuestionnairePage} />
+                <Route exact path="/questionnaire/result" component={ResultPage} />
+                <Route exact path="/Regions" component={RegionPage} />
+                <Route exact path="/:City" component={CityPage} />
+                <Route exact path="/:City/:Club" component={ClubPage} />
+                <Route exact path="/:City/Sport/:Sport" component={SportPage} />
+                <Route exact path="/:City/:Club/:Group" component={GroupPage} />
+            </Switch>
         </Router>
     </Provider>
 );
 
-export default createRoutes;
+export default Routes;
