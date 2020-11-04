3b421647b4c6bef2733f376f252e13aa84f74448
KristofferHaakonsen <kristofferhhaakonsen@gmail.com>
2020-11-04 15:59:44 +0100

    Readd maxwidth

--- src/styles/card.ts
+++ src/styles/card.ts
@@ -14,6 +14,7 @@ export const cardList = style(
 export const card = style({
     margin: '0px 0px 0px 0px',
     width: '100%',
+    maxWidth: '315px',
     backgroundColor: colors.white,
     filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
     borderRadius: '5px',
