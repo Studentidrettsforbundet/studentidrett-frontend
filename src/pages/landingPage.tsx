9441136fdb32dcc2cb21cde5341c4a0084ce34ea
eirsture <37185897+eirsture@users.noreply.github.com>
2020-11-04 15:17:16 +0100

    style front page (#51)
    * style front page
    
    * update snapshot test

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -1,23 +1,60 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { style, classes } from 'typestyle';
+import colors from '../styles/colors';
 
 const LandingPage = () => {
     return (
-        <div className="Landing">
-            <div className="col">
-                <button type="button" className="btn btn-primary">
-                    <Link to="/Regions" style={{ color: 'black' }}>
-                        Regioner
-                    </Link>
-                </button>
-                <button type="button" className="btn btn-primary">
-                    <Link to="/questionnaire" style={{ color: 'black' }}>
+        <div className={frontPage}>
+            <div className={wrapper}>
+                <p>
+                    Norges Studentidrettsforbund har et bredt idrettstilbud rundt i hele Norge gjennom våre
+                    studentidrettslag. På denne nettsiden kan du finne hva vi tilbyr i din studieby!
+                </p>
+                <Link to="/Regions" style={{ color: colors.secondary }} className="btn btn-default">
+                    <button type="button" className="btn btn-default">
+                        Finn idrett
+                    </button>
+                </Link>
+            </div>
+            <div className={wrapper}>
+                <p>
+                    Usikker på hva du ønsker å drive med på fritiden, eller ønsker du å vite hvilke idretter som kunne
+                    passet til deg? Vi har laget en idrettsvalgomat som foreslår idretter vi tenker du kunne likt!
+                </p>
+                <Link to="/questionnaire" style={{ color: colors.secondary }}>
+                    <button type="button" className="btn btn-default">
                         Idrettsvalgomat
-                    </Link>
-                </button>
+                    </button>
+                </Link>
             </div>
         </div>
     );
 };
 
 export default LandingPage;
+
+const frontPage = style({
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+});
+
+const wrapper = classes(
+    frontPage,
+    style({
+        textAlign: 'center',
+        maxWidth: '550px',
+        $nest: {
+            '&>a>button': {
+                marginBottom: '6vh',
+                backgroundColor: colors.primary,
+                borderColor: colors.primary,
+                filter: 'drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))',
+            },
+            '&>a>': {
+                textDecoration: 'none',
+            },
+        },
+    }),
+);
