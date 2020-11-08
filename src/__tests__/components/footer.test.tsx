a0bad2fdd6c6abc1aba076c4161e19370e55c26e
magdalenatran <tranmagdalena@gmail.com>
2020-11-08 02:44:16 +0100

    update footer with content

--- src/__tests__/components/footer.test.tsx
+++ src/__tests__/components/footer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Footer from '../../components/footer';
+import Footer from '../../components/Footer/footer';
 
 describe('footer', () => {
     describe('snapshot tests', () => {
